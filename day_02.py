98bd354857dff5239ab4a8c688f8ffe8f79434a5
inhauni <inhauni123@gmail.com>
2023-01-13 10:55:49 +0900

    day02 datatype dictionary

---
+++ day_02.py
@@ -0,0 +1,13 @@
+harry_porter={
+    '헤르미온느' : '그리핀도르',
+    '말포이' : '슬리데린',
+    '말포이' : '그리핀도르'
+}
+
+print(harry_porter)
+print(type(harry_porter))
+
+#출력값 :
+#{'헤르미온느': '그리핀도르', '말포이': '그리핀도르'} -> 말포이라는 key값의 최종 value는 가장 마지막에 입력된 값
+#<class 'dict'>
+
