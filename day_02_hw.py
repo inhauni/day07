d5bc2466285090f28d887b3c8c55045054da4ca2
inhauni <inhauni123@gmail.com>
2023-01-13 15:39:45 +0900

    first question

---
+++ day_02_hw.py
@@ -0,0 +1,10 @@
+import random
+
+guess = int(input("1에서 10까지의 정수 중 하나를 입력하세요 : "))
+secret = random.randint(1, 10)
+if guess > secret:
+    print(f'{guess} is too high')
+elif guess < secret:
+    print(f'{guess} is too low')
+else:
+    print(f'{guess} is just right')
