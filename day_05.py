90d12715ccb81b77fe0f2714c3cbed63783ef9ee
inhauni <inhauni123@gmail.com>
2023-01-18 13:02:10 +0900

    calculate fee

--- day_05.py
+++ day_05.py
@@ -1,28 +1,33 @@
-# function : prime number
-def isprime(n):
+# None
+
+# def do_nothing():
+#     pass
+
+# print(do_nothing())
+
+# mamamoo =['화사','솔라','휘인','문별']
+
+# # print(mamamoo.pop()) # 삭제 할 값 리턴 후 삭제
+# print(mamamoo.remove('문별')) # 삭제만 하고 리턴하지 않음 -> None 출력 none: 아무 것도 없다
+# print(mamamoo) #문별을 제외한 list가 출력됨
+
+
+def calculate_fee(*args):
+
+    '''
+    [놀이공원 요금계산 프로그램]
+    :param args: 나이
+    :return: 총 지불 요금
     '''
 
-    :param n: integer number (정수)
-    :return: True or False (소수일 때는 True를 return 해줌)
-    '''
+    total = 0
+    for age in args:
+       if age >= 19: #adult
+           total= total+10000
+       else:
+           total=total+3000
 
-    if n <= 1: # 1은 소수가 아니므로 False 반환
-        return False
-    for k in range(2, n): # 소수가 아닌 경우
-        if n % k == 0:
-            return False
-    else: # 소수일 경우
-        # print(i, end=' ') 함수 하나는 한 가지 역할에 충실 해야 함 : 답변(True or False)과 출력을 동시에 할 수 없다
-        return True
+    return total
 
-
-
-start = int(input("input start number : "))
-end = int(input("input end number : "))
-
-if end < start:
-    start, end = end, start
-
-for i in range(start, end+1):
-    if isprime(i):
-        print(i, end=' ')
+print(calculate_fee(20,25,25))
+print(calculate_fee(10,7,52,43))
