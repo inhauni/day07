1c7ea2695d69f83b02b7b39849340d715421c6c3
inhauni <inhauni123@gmail.com>
2023-01-18 15:44:27 +0900

    lambda

--- day_05.py
+++ day_05.py
@@ -1,34 +1,31 @@
-# Closures : 일반 함수와 다르게 외부 함수의 변수 값을 기억 하고 있다
+# 일반적인 Function
 
-def calculate():
-    x = 1
-    y = 2
+import random
 
-    def add_sub(n): # x값과 y값(외부함수로부터 생성된 변수값)을 기억하고 있는 closure(고차함수)
+# def process(no_lists,f):
+#     for n in no_lists:
+#         print(f(n))
+# def squares(n):
+#     '''
+#
+#     제곱 함수
+#     :param n: 정수
+#     :return: 정수(integer)
+#     '''
+#
+#     return n*n
 
-        # x=11 : 외부함수의 변수값을 변경하고 저장 -> 이는 calculate 함수의 x=1과 다르다 \
-                # 왜냐하면 x=11의 x는 local variable (지역변수) 이므로 함수 밖에선 효력을 상실한다.
 
-        return x + n - y
-    print('once') # 한번만 출력됨!!! add_sub로 들어가서 거기서 for문이 돌아감
-    return add_sub
+#Lambda
+def process(no_lists,f): # f에 lambda x: x*x 라는 익명함수(람다함수)가 대입 됨
+    for n in no_lists:
+        print(f(n))
 
 
-c1 = calculate() # 함수를 변수에 대입
 
-for i in range(5):
-    print(c1(i)) # add_sub(i)와 같은 의미
+numbers=[random.randint(1, 10) for i in range(5)]
+print(numbers)
 
-#nonlocal : 지역함수 해제
-def calculate():
-    x = 1
-    y = 2
-    temp = 0
-    def add_sub(n):  # x값과 y값(외부함수로부터 생성된 변수값)을 기억하고 있는 closure(고차함수)
-        nonlocal temp
-        # x=11 : 외부함수의 변수값을 변경하고 저장 -> 이는 calculate 함수의 x=1과 다르다 \
-        # 왜냐하면 x=11의 x는 local variable (지역변수) 이므로 함수 밖에선 효력을 상실한다.
-        temp = temp + x +n - y # 이전의 temp값이 남아있어 여기에 대입 된다
-        return temp
+process(numbers, lambda x:x*x) # 람다라는 익명 함수 : x를 대입 시에 x*x를 retrun
 
-    return add_sub
+
