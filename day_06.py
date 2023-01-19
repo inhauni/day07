9ac1f6965b15248860d9bbbda973bc13d2fb4ce2
inhauni <inhauni123@gmail.com>
2023-01-19 14:48:50 +0900

    예제 문제

--- day_06.py
+++ day_06.py
@@ -1,24 +1,15 @@
-# <예외 만들기>
+# 파이썬 예제 문제
 
-try :
-    # raise Exception("쉬는 시간!")
-    raise TypeError('타입에러') # Excetion 이 other로 받아서 출력
-    expr = input('분자 분모 입력 (띄어쓰기로 구분) : ').split()
-    print(int(expr[0])/int(expr[1]))
+groups ={
+    '빅뱅':['지디','태양','탑','대성','승리',],
+    '마마무' :['화사','문별','솔라','휘인']
+}
 
+for group, mems in groups.items():
 
+    print(f'{group}의 멤버:')
 
-except ValueError as err:
-    print(f'{err}, 숫자를 입력해주세요!')
-except ZeroDivisionError as err:
-    print(f'{err}, 분모에 0이 올 수 없습니다!')
-except IndexError as err:
-    print(f'{err}, 인덱스의 범위에 문제가 있습니다!')
-except Exception as other:
-    print(f'{other}, 예외 발생')
+    for mem in mems:
+        if mem != '승리':
+            print(mem)
 
-else:
-    print('프로그램 정상',end=' ')
-
-finally:
-    print('종료')
