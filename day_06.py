8978bdb29abdd1d74f7ae3c35e04161e408dfb32
inhauni <inhauni123@gmail.com>
2023-01-19 11:06:05 +0900

    namespace 1

--- day_06.py
+++ day_06.py
@@ -1,32 +1,19 @@
+# 전역 함수
+
+# g =1
+#
+# def print_global():
+#     print(g)
+#
+# print(g)
+# print_global() #두개 다 출력 됨
 
 
-def document_info(func):
-    def new_function(*args,**kwargs):
-        print('실행중인 함수: ',func.__name__)
-        print('위치 기반 인자들: ',args)
-        print('키워드 기반 인자들: ', kwargs)
-        result =func(*args,**kwargs)
-        print('실행결과: ',result)
-        return result
-    return new_function
+# def print_global():
+#     g = 1
+#     print(g)
+#
+# print_global()
+# print(g) # Error 발생 'g' 는 전역변수가 아니므로 (지역변수), 함수 밖에선 보이지 않는다
+#
 
-
-def square_it(func):
-    def new_function(*args,**kwargs):
-        result=func(*args,**kwargs)
-        return result * result
-    return new_function
-
-@document_info
-@square_it
-def add_int(a,b):
-    return a+b
-
-print(add_int(3,5)) # 이 때 print 함수는 return 값을 출력
-# add_int(3,5) : 이거는 return 값이 출력되지 않음
-@square_it
-@document_info
-def add_int(a,b):
-    return a+b
-
-print(add_int(3,5))
